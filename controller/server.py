cf810db9d9180594ef1c463989784180227ecdb8
sasanquaneuf <sasanquaneuf@gmail.com>
2020-03-09 13:34:57 +0900

    githubのプルリクエストに合わせてDBデータを入れる（中央サーバー側）※メール送信等はまだ

--- controller/server.py
+++ controller/server.py
@@ -15,7 +15,7 @@ app = Blueprint(__name__, "server")
 
 @app.route('/')
 def route():
-    return 'hello!'
+    return render_template('top.html')
 
 
 @app.route('/server/list')
@@ -88,8 +88,13 @@ def server_stop():
 @app.route('/pull/list')
 @need_basic_auth
 def pull_list():
+    return json.dumps(check_and_update_pull_request())
+
+
+def check_and_update_pull_request():
     basic_digest = base64.b64encode(
         (webapp_settings["owner"] + ':' + webapp_settings["token"]).encode()).decode()
+    # TODO  too many pull_requests
     req = urllib.request.Request(
         f'https://api.github.com/repos/{webapp_settings["owner"]}/{webapp_settings["repo"]}/pulls?state=all',
         headers={'Authorization': f'Basic {basic_digest}'}
@@ -97,7 +102,7 @@ def pull_list():
     with urllib.request.urlopen(req) as res:
         pull_request_list = json.loads(res.read())
 
-    pull_request_list_new = [{
+    github_pull_request_list = [{
         'number': p['number'],
         'state': p['state'],
         'sha': p['head']['sha'],
@@ -105,4 +110,44 @@ def pull_list():
         'ref': p['head']['ref'],
     } for p in pull_request_list]
 
-    return json.dumps(pull_request_list_new)
+    boto = BotoSession(
+        region_name='ap-northeast-1',
+        aws_access_key_id=webapp_settings['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=webapp_settings['AWS_SECRET_KEY'])
+    ec2 = boto.client('ec2')
+
+    with Connection() as cn:
+        db_pull_request_list = cn.s.query(PullRequest).filter(
+            PullRequest.number.in_([p['number'] for p in github_pull_request_list])).all()
+        # insert pull requests
+        db_number_dict = {p.number: p for p in db_pull_request_list}
+        stopped_server_list = cn.s.query(Server).filter(
+            ~cn.s.query(PullRequest).filter(
+                PullRequest.server_id == Server.id, PullRequest.state != 'open').exists()
+        )
+        for pull_request in github_pull_request_list:
+            if pull_request['number'] not in db_number_dict.keys():
+                # insert
+                new_db_pull_request = PullRequest(
+                    number=pull_request['number'],
+                    state=pull_request['state'],
+                    sha=pull_request['sha'],
+                    title=pull_request['title'],
+                    ref=pull_request['ref'],
+                )
+                if new_db_pull_request.state == 'open':
+                    server = stopped_server_list.pop()
+                    new_db_pull_request.server_id = server.id
+                    ec2.start_instances(InstanceIds=[server.instance_id])
+                cn.s.add(new_db_pull_request)
+            else:
+                # update (if status was changed)
+                db_pull_request = db_number_dict[pull_request['number']]
+                if db_pull_request.state != pull_request['state']:
+                    if db_pull_request.state == 'open':
+                        ec2.stop_instances(InstanceIds=[db_pull_request.server.instance_id])
+                    db_pull_request.state = pull_request['state']
+                    cn.s.add(db_pull_request)
+        cn.s.commit()
+
+    return github_pull_request_list
