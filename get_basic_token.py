6c9fda5526f4f9b043e0b7e77f370b94b112c663
sasanquaneuf <sasanquaneuf@gmail.com>
2020-03-23 01:35:50 +0900

    add get_basic_token tool

---
+++ get_basic_token.py
@@ -0,0 +1,8 @@
+import hashlib
+
+
+if __name__ == '__main__':
+    raw_password = input('input raw password:')
+    print(raw_password)
+    print('the token is ...')
+    print(hashlib.sha512(raw_password.encode()).hexdigest())
