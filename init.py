af7ebfb3d2079227209700b1ec983af4598656b5
sasanquaneuf <sasanquaneuf@gmail.com>
2020-05-05 12:50:27 +0900

    fix: exec shell with sh

--- init.py
+++ init.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     with open('alembic/alembic.ini', 'w') as f:
         f.write(''.join(lines).replace('[mysql_connection]', webapp_settings['mysql_connection']))
     try:
-        subprocess.check_output('./init_db.sh', shell=True)
+        subprocess.check_output('sh ./init_db.sh', shell=True)
     except Exception as ex:
         print(ex.__dict__)
         raise ex
