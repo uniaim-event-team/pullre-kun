b17686b41aede0f02f84938a597f1f5a8c7f18e5
sasanquaneuf <sasanquaneuf@gmail.com>
2021-02-19 00:54:33 +0900

    fix: get_issue fails if there are commits more than 100

--- batch/get_issue.py
+++ batch/get_issue.py
@@ -13,5 +13,12 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     gc = GitHubConnector()
-    result = gc.save_all_issues(args.p, args.s)
-    pprint(result)
+    pages = args.p
+    # TODO adjust trial count (or fix except)
+    for i in range(pages, pages + 10):
+        try:
+            result = gc.save_all_issues(i, args.s)
+            pprint(result)
+            break
+        except Exception as ex:
+            print(ex)
